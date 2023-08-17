7e7ddd47253c72ac7985c2a8f7630e6ae3485649
alessandropecchini99 <pecchini99@gmail.com>
2023-08-17 11:37:06 +0200

    finish first routes

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,14 @@
-<script></script>
+<script>
+import AppNav from "./components/AppNav.vue";
+export default {
+  components: {
+    AppNav,
+  },
+};
+</script>
 
 <template>
+  <AppNav />
   <router-view></router-view>
 </template>
 
